754d7493e2a4fb4950531891c4aaa8289bfe2077
Irving Rabel <irabel@jacksontherpay.com>
2026-07-20 23:58:15 -0400

    Feat: Added create and show actions

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,26 @@ class Api::V1::UsersController < ApplicationController
     users = User.all
     render json: users
   end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User not found" }, status: :not_found
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :role, :coach_id)
+  end
 end
