b2ef62019b7478d06db01d7328c0b941041f7589
Irving Rabel <irabel@jacksontherpay.com>
2026-07-21 22:03:32 -0400

    Feat: added validations to user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   belongs_to :coach, class_name: "User", optional: true
   has_many :clients, class_name: "User", foreign_key: "coach_id"
   has_many :check_ins, dependent: :destroy
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true
 end
